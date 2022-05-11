90087a80c023b9b1dbd716c68f94010756e3cb49
Mosrur Mohboob Chowdhury <mosrur@gmail.com>
2022-05-11 12:46:56 -0400

    Update userscript.js

--- userscript.js
+++ userscript.js
@@ -2,13 +2,12 @@
 // @name style-json-formatter 
 // @namespace https://github.com/mosrur/JSON-Formatter-Userscript/
 // @version 1.0
-// @description custom styling for JSON formatter in dark mode 
+// @description Hack to fix dark mode for JSON formatter extension. 
 // @author Mosrur 2022-05-11 12:30:36 PM 
 // @include /^http(s)?\:\/\/.*\.json(\?\=.*\b)?$ 
 // @grant none
 // ==/UserScript== 
 
 (function() { 'use strict'; 
-// Hack to fix dark mode for this extension. 
   GM_addStyle( [ 'body { background-color: white; }' ].join(' ') ); 
 })();
