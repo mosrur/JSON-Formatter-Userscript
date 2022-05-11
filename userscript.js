d28e81fd2d16e86a65d521990d0dbd98d0212cd9
Mosrur Mohboob Chowdhury <mosrur@gmail.com>
2022-05-11 12:36:06 -0400

    Create userscript.js

---
+++ userscript.js
@@ -0,0 +1,9 @@
+// ==UserScript== 
+// @name style-json-formatter 
+// @namespace https://github.com/cookch10 
+// @version 0.1 
+// @description custom styling for JSON formatter in dark mode 
+// @author Mosrur 2022-05-11 12:30:36 PM 
+// @include /^http(s)?\:\/\/.*\.json(\?\=.*\b)?$ 
+// @grant GM_addStyle // ==/UserScript== (function() { 'use strict'; 
+// Hack to fix dark mode for this extension. GM_addStyle( [ 'body { background-color: white; }' ].join(' ') ); })();
